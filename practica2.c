2ffe34928acaa56a6e9c79f6b7a6f0cab95149b8
Jose Lino Carrillo Villalobos <96278834+LinoCarrillo@users.noreply.github.com>
2023-08-31 10:45:21 -0600

    31 agosto
    actualización de documentos

---
+++ practica2.c
@@ -0,0 +1,12 @@
+// manejo de if como estructura de control básica
+// Fecha: 21 agosto 2023
+#include <stdio.h>
+int main()
+{
+    int precio=500;
+    float descuento;
+    if (precio>=500)
+        printf("el descuento es del 20");
+    else
+        printf("Sin descuento");
+}
